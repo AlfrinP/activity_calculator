b476164735c9f7d60b929c5a9dd7872ccd0602ac
AlfrinP <alfrinpoulose@gmail.com>
2024-01-12 11:59:49 +0530

    made logout work

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,38 @@
 import React from "react";
 import logout from "../assets/General/logout.png";
 import christ from "../assets/General/christ.png";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { baseURL } from "./Util";
 
 function Navbar() {
+  const navigate = useNavigate();
+
+  async function handleLogout() {
+    try {
+      const response = await axios.post(
+        `${baseURL}logout`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      console.log(response.data.student);
+      localStorage.removeItem("token");
+      navigate("/login");
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
   return (
     <div className="h-[100px] center w-full">
       <div className="justify-between max-w-7xl w-full center p-1">
-        <img src={christ} />
-        <button className="center justify-end gap-1 p-3">
-          <img src={logout} />
+        <img src={christ} alt="Logo" />
+        <button className="center justify-end gap-1 p-3" onClick={handleLogout}>
+          <img src={logout} alt="Logout Icon" />
           <div className="text-lg text-[#512B81] font-semibold">Logout</div>
         </button>
       </div>
--- server/todo.md
+++ server/todo.md
@@ -7,6 +7,11 @@ FIX:
 -   the student register must have only : email, reg_no, name and password
 <!-- -   the student model should also have faculty name along with id -->
 -   when in dashboard shouldnt go to login page
+- urls needed for get all certificates with yearwise filtering and total points.
+- urls for certificate specific view.
+- uploaded details, no need to edit, no need for cancel & save.
+- need to fix nss,ncc points.(frontend)
+<!-- - need to give logout url in frontend. -->
 
 TODO:
 certificate: https://www.youtube.com/watch?v=l2v_yIfGmho
