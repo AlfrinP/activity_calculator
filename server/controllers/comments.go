d708bfee7ccb53f1783d932d89ab175490dc7911
rinto11804 <rinto11804@gmail.com>
2024-01-27 11:54:17 +0530

    refactor(server): corrected error messages in comments.go

--- server/controllers/comments.go
+++ server/controllers/comments.go
@@ -13,41 +13,42 @@ import (
 func PostCommentWithStatusChange(c *fiber.Ctx) error {
 
 	f, _ := c.Locals("user").(*util.Data)
+	
 	if f == nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"error": "user is not allowed",
 		})
 	}
 
-	if f.Role == "faculty" {
-		params := &models.CommentCreate{}
-
-		if err := c.BodyParser(params); err != nil {
-			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-				"error": "error in parsing request data ",
-			})
-		}
-
-		if err := params.Validate(); err != nil {
-			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-				"error": "error in validating request data ",
-			})
-		}
-		log.Println(params)
-
-		certificateRepo := repository.NewCertificateRepository(storage.GetDB())
-		if err := certificateRepo.ChangeStatusComment(params.CertificateID, params.Status, params.Message); err != nil {
-			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-				"error": "certificate status and comment update failed",
-			})
-		}
-
-		return c.Status(fiber.StatusCreated).JSON(fiber.Map{
-			"msg": "Comment and status changed",
-		})
-	} else {
+	if f.Role != "faculty" {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"error": "role is not allowed",
 		})
 	}
+
+	params := &models.CommentCreate{}
+
+	if err := c.BodyParser(params); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "failed in parsing request data ",
+		})
+	}
+
+	if err := params.Validate(); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "failed in validating request data ",
+		})
+	}
+	log.Println(params)
+
+	certificateRepo := repository.NewCertificateRepository(storage.GetDB())
+	if err := certificateRepo.ChangeStatusComment(params.CertificateID, params.Status, params.Message); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "certificate status and comment update failed",
+		})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"msg": "Comment and status changed",
+	})
 }
